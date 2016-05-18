e5f53092f5470644ad15c6a490015c7d133d543b
Boubacar Diarra <boubadiarra@gmail.com>
2016-05-18 22:46:57 +0200

    house

--- DLibSolution/test_infostat/test_memorystatstore.cpp
+++ DLibSolution/test_infostat/test_memorystatstore.cpp
@@ -23,7 +23,6 @@ public:
   ////////////////////////////////////
 BOOST_FIXTURE_TEST_SUITE(MemoryStoreTestSuite, MemoryStoreFixture);
 BOOST_AUTO_TEST_CASE(testDatasets) {
-	
 } // testDatasets
 BOOST_AUTO_TEST_CASE(testVariables) {
 	
